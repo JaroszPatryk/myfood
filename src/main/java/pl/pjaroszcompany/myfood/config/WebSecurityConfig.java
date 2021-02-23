9ca75a5f679f41e2f4f3a5e40a937ce51d969ab0
pjaro <dzejpi27@gmail.com>
2021-02-23 15:01:51 +0100

    Spring security - user from database part 2

--- src/main/java/pl/pjaroszcompany/myfood/config/WebSecurityConfig.java
+++ src/main/java/pl/pjaroszcompany/myfood/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final SecurityUserDetailsService securityUserDetailsService;
@@ -39,9 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/mvc/register").permitAll()
-                .antMatchers("/mvc/meal", "mvc/meal/search").authenticated()
-                .antMatchers("/mvc/meal/add", "mvc/meal/delete/**", "/mvc/meal/edit").hasRole("ADMIN")
+                .antMatchers("/**").permitAll()
                 .and()
                 .formLogin()
                 .successForwardUrl("/mvc/meal")
--- src/main/java/pl/pjaroszcompany/myfood/web/MealController.java
+++ src/main/java/pl/pjaroszcompany/myfood/web/MealController.java
@@ -1,20 +1,16 @@
 package pl.pjaroszcompany.myfood.web;
 
 import lombok.AllArgsConstructor;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import pl.pjaroszcompany.myfood.domain.meal.Meal;
 import pl.pjaroszcompany.myfood.domain.meal.MealService;
 import pl.pjaroszcompany.myfood.products.SearchParams;
 
-import javax.validation.Valid;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
+
 
 
 @Controller
